754f4a3bca383dcccb41e4a020852a7a057ef2f3
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-06 10:03:58 -0700

    sas: refactored input from command line

--- sas/main.go
+++ sas/main.go
@@ -5,16 +5,12 @@ import (
 )
 
 func main() {
-	var n int
 	fmt.Println("Enter number of values, then the values:")
 	fmt.Println("n")
 	fmt.Println("n1 n2 n3 . . .")
-	fmt.Scanln(&n)
-
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&arr[i])
-	}
+	size := readSize()
+	arr := make([]int, size)
+	readElements(arr)
 	sum := SimpleArraySum(arr)
 	fmt.Println("sum:", sum)
 }
@@ -26,4 +22,16 @@ func SimpleArraySum(ints []int) (sum int) {
 		sum += v
 	}
 	return
+}
+
+func readSize() int {
+	var n int
+	fmt.Scanln(&n)
+	return n
+}
+func readElements(arr []int) {
+	size := len(arr)
+	for i := 0; i < size; i++ {
+		fmt.Scan(&arr[i])
+	}
 }
