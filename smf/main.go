9cc65cc0edc6f6a859304e2906b9dbcb4eb7ca43
pepe <jose@adminisatorsmbp.lan>
2018-10-05 22:23:23 -0700

    hardcode default values

--- smf/main.go
+++ smf/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var a, b, sum int
+	sum = solveMeFirst(a, b)
+	fmt.Println(sum)
+}
+
+func solveMeFirst(a, b int) (sum int) {
+	sum = a + b
+	return
 }
